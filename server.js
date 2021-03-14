af820d008008087289a93cd5b6aae077cdf238e6
Kirito <kirito61254@gmail.com>
2021-03-14 13:09:49 +0900

    1.5 Prisma Migrate

--- server.js
+++ server.js
@@ -1,20 +1,43 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
-    type Query {
-        hello: String
-    }
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
 `;
 
 const resolvers = {
-    Query: {
-        hello: () => "hiiii",
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
     },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+  },
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
 
-server.listen().then(() => console.log("Server is running on http://localhost:4000/"));
+server
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
