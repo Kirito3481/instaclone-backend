a1abc419ffe6a84b8437249ef4077d80605d5636
Kirito <kirito61254@gmail.com>
2021-03-16 13:12:19 +0900

    #2.14 File Upload part One

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 require("dotenv").config();
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
 import client from "./client";
-import schema from "./schema";
-import { getUser, protectResolver } from "./users/users.utils";
+import { typeDefs, resolvers } from "./schema";
+import { getUser } from "./users/users.utils";
 
 const server = new ApolloServer({
-  schema,
+  typeDefs,
+  resolvers,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
       client,
-      protectResolver,
     };
   },
 });
